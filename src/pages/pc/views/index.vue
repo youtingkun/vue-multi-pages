9e71713d9a4053e1383b845b311e6a6a19f92782
youtingkun <youtingkun@techsailing.com>
2021-04-20 16:23:33 +0800

    init

---
+++ src/pages/pc/views/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="">pc-views-index
+        <el-button>按钮</el-button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: '',
+    components: {},
+    props: {},
+    data() {
+        return {
+
+        };
+    },
+    computed: {},
+    watch: {},
+    methods: {},
+    created() {},
+    mounted() {},
+    beforeCreate() {},
+    beforeMount() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeDestroy() {},
+    destroyed() {},
+    activated() {},
+    }
+</script>
+<style lang='scss' scoped>
+//@import url(); 引入公共css类
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    pages: {
+        'mobile': { // mobile
+            entry: 'src/pages/mobile/index.js',
+            template: 'src/pages/mobile/mobile.html',
+            filename: 'mobile/index.html',
+            title:'mobile'
+          },
+      'pc': { // pc
+        entry: 'src/pages/pc/index.js',
+        template: 'src/pages/pc/index.html',
+        filename: 'mobile/index.html',
+        title:'pc'
+      },
+     
+    }
+}
